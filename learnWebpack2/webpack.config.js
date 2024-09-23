916a332ed49fe0800b4043041549d04372d34c89
ducksfrogs <ma_yamaki@yahoo.com>
2024-09-23 16:23:48 +0900

    first jdafaksf

--- learnWebpack2/webpack.config.js
+++ learnWebpack2/webpack.config.js
@@ -1,9 +1,32 @@
 const path = require('path');
+const HtmpwebpackPlugin = require("html-webpack-plugin");
 
 module.exports = {
-    entry: './src/index.js',
+    mode: 'development',
+    entry: {
+        index: './src/index.js',
+        another: './src.another-module.js',
+    },
+    plugins: [
+        new HtmpwebpackPlugin({
+            title: 'output management',
+        }),
+    ],
     output: {
-        filename: 'main.js',
+        filename: '[name].bundle.js',
         path: path.resolve(__dirname, 'dist'),
-    }
-}
+    },
+    module: {
+        rules: [
+            {
+                test: /\.css%/i,
+                use: ['style-loader', 'css-loader'],
+            },
+
+            {
+                test: /\.(png|svg|jpeg|jpg|gif)%/i,
+                type: 'asset/resource',
+            },
+        ],
+    },
+};
